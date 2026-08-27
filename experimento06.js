7b29d70d14387fc114766a92d7ca3ed77fb04991
Leonardoschmoeller <leonardo.rodrigues.souza31@escola.pr.gov.br>
2026-08-27 11:05:20 +0000

    atualização

--- experimento06.js
+++ experimento06.js
@@ -1,6 +1,17 @@
-const botaoAbrir = document.getElementById("btnAbrieModal");
+const botaoAbrir = document.getElementById("btnAbrirModal");
 const modalCotainer = document.getElementById("modalContainer");
 
 botaoAbrir.addEventListener("click", function(){
-    
+    modalCotainer.innerHTML = `
+    <div class="modal-fundo">
+        <div class="modal-caixa">
+          <h2>Aviso!</h2>
+          <p> este modal inteiro foi criado pelo JavaScript.</p>
+          <button id="btnFecharModal">Fecha</button>
+        </div>
+    </div>`;
+    const botaoFechar = document.getElementById("btnFecharModal");
+     botaoFechar.addEventListener("click", function(){
+        modalCotainer.innerHTML = "";
+    });
 })
